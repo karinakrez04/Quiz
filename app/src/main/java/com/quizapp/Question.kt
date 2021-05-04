0b1268c516cda6971d23421cdb9471b9f5b51ea8
aleksandr.kulinka <aleksandr.kulinka@effective-soft.com>
2021-05-04 23:18:27 +0300

    ImageLink field to load with Glide and share link with friends

--- app/src/main/java/com/quizapp/Question.kt
+++ app/src/main/java/com/quizapp/Question.kt
@@ -4,6 +4,7 @@ data class Question(
     val id: Int? = null,
     val question: String? = null,
     val image: Int? = null,
+    val imageLink: String? = null,
     val optionOne: String? = null,
     val optionTwo: String? = null,
     val optionThree: String? = null,
